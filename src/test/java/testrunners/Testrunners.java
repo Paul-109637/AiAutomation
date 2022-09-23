fc535d8cd421e467b181afbe5c773381fd7c7d9c
Paul-109637 <113929951+Paul-109637@users.noreply.github.com>
2022-09-23 09:39:49 +0530

    Update Testrunners.java

--- src/test/java/testrunners/Testrunners.java
+++ src/test/java/testrunners/Testrunners.java
@@ -5,13 +5,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-//@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(features="Featurefiles/RecruiterAiFeatures.feature",glue="stepdefinitions",
 format= {"pretty","html:test-output_1",
                 "json:target/cucumber-reports/CucumberTestReport.json"},
 plugin={"json:target/cucumber-reports/CucumberTestReport.json"},dryRun=false,
 tags={"@RecruiterAiLogin,@RecruiterAiLogout,@CreateNewJob"})
-public class Testrunners extends AbstractTestNGCucumberTests
+public class Testrunners
 {
 
 }
